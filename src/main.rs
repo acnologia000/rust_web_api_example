e058c81c138a944f2e89175b691da6bff2186289
aconologia000 <hridyanshbluedevil@hotmail.com>
2019-08-18 12:18:06 +0530

    error handling almost done in main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,16 @@ fn main() {
     //using 2 threads per route as default in case variable is not set
     //this can be hardcoded or read from a file , hence optional
     let pool_size : usize = match env::var("THREAD_POOL_SIZE_FOR_EACH_ROUTE"){
-        Ok(var)=> var.parse().expect("THREAD_POOL_SIZE_FOR_EACH_ROUTE can only be a unsigned integer") ,
+        // value of 'THREAD_POOL_SIZE_FOR_EACH_ROUTE' is returned as string that needs to 
+        // be parsed into usize ,in case of errors we are using default values
+        Ok(var)=> match var.parse(){
+            Ok(val) => val,
+            Err(_err) => {
+                println!("> Parse Error :{}'THREAD_POOL_SIZE_FOR_EACH_ROUTE' can only have unsigned integer Value",_err);
+                println!("> using default value for THREAD_POOL_SIZE_FOR_EACH_ROUTE");
+                2
+            }
+         },
         Err(_s)=>2
     };
 
@@ -20,17 +29,26 @@ fn main() {
     //this can be hardcoded or read from a file , hence optional
     let port = match env::var("RUST_SERVER_PORT"){
         Ok(var)=> var ,
-        Err(_s)=>"0.0.0.0:7878".to_string()
+        Err(_s)=>{
+            println!("> failed at reading :{} 'RUST_SERVER_PORT' using default",_s);
+            "0.0.0.0:7878".to_string()
+            }
     };
 
     //spitting basic chatter to notify user that application is running and reporting settings being used 
     // again totally optional but helpful  
     println!("> edit 'RUST_SERVER_PORT' environment variable to change server listening port");
     println!("> edit 'THREAD_POOL_SIZE_FOR_EACH_ROUTE' environment variable to change thread pool size");
-    println!("> Using {} as default thread pool size for each route \n> using {} as port",pool_size,port);
+    println!("> Using {} as thread pool size for each route \n> using {} as port",pool_size,port);
 
     // binding a listner on our designated port for listening for Tcp requests 
-    let listner  = TcpListener::bind(&port).expect("binding Failure");
+    // binding to a port might fail in case if we are bining to port that needs
+    // elivated privelleges to access or port is busy(being used by another application)
+    // or port/Ip are unavailable or wrong , its a good idea to report details to user 
+    let listner  = match TcpListener::bind(&port){
+        Ok(val) => val,
+        Err(err) => panic!("> Binding failure : {}",err)
+    };
     
     //declaring string pattern for all routes 
     let home_route = String::from_utf8_lossy(b"/").to_string();
@@ -55,11 +73,17 @@ fn main() {
     for stream in listner.incoming() {
         
         // getting actual Tcp::stream from Result type given by listener
-        let mut stream = stream.unwrap();
+        let mut stream = match stream {
+            Ok(val) => val,
+            Err(_err) => {panic!("> Failed at Unwrapping Stream :{}",_err)}
+        };
 
         // stream does not returns Tcp request directly , instead it writes it into 
         // empty byte array we provide 
-        stream.read(&mut req_buffer).unwrap();
+        match stream.read(&mut req_buffer){
+            Ok(_val) => {},
+            Err(err) => println!("> Failed at reading Request into buffer :{}",err)
+        };
         
         // parsing request (which is stored in req_buffer) from [u8] to more readable and usable data structure 
         let request = request_proc::parse_request(&mut String::from_utf8_lossy(&req_buffer).to_string()).unwrap();
@@ -74,7 +98,7 @@ fn main() {
         ref path if path.path == route_3 => route3pool.execute(|| route2(stream)),
 
         // _ handles all the cases that cannot be handled in our defined paths 
-        // since we dont have what use is asking for so according to internet standard
+        // since we dont have what user is asking for so according to internet standard
         // we will return Error 404
         // we will send response by stream.write(b"some response") method in stream 
         // response is always written as &[u8] (refrance to byte array) 
@@ -83,7 +107,6 @@ fn main() {
         // if everything goes well it returns number bytes sent as response (which is useless in most cases)
         _ => {stream.write(b"Error 404").unwrap();}
         }
-                
     }
 }
 
