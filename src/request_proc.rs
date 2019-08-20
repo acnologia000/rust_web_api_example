1650f52d64f714b9200efa7ffee28154ab818e88
aconologia000 <hridyanshbluedevil@hotmail.com>
2019-08-20 13:05:55 +0530

    request type enum added

--- src/request_proc.rs
+++ src/request_proc.rs
@@ -1,6 +1,18 @@
+use std::fmt;
+
+pub enum RequestType {
+    GET,
+    POST,
+    PUT,
+    HEAD,
+    DELETE,
+    PATCH,
+    OPTIONS,
+}
+
 pub struct Request {
     pub http_version: String,
-    pub method: String,
+    pub method: RequestType,
     pub path: String,
 
     pub gzip: bool,
@@ -11,7 +23,7 @@ pub struct Request {
 pub fn parse_request(request: &mut String) -> Result<Request, ()> {
     let mut parts = request.split(" ");
 
-    let method = parts.next().unwrap().to_string();
+    let method = get_request_type(parts.next().unwrap().to_string()).unwrap_or(RequestType::GET);
     let path = parts.next().unwrap().to_string();
     let http_version = parts.next().unwrap().to_string();
 
@@ -68,6 +80,40 @@ impl Request {
         );
     }
 }
+
+pub fn get_request_type(req: String) -> Option<RequestType> {
+    if req.to_uppercase().contains("GET") {
+        return Some(RequestType::GET);
+    } else if req.to_uppercase().contains("POST") {
+        return Some(RequestType::POST);
+    } else if req.to_uppercase().contains("DELETE") {
+        return Some(RequestType::DELETE);
+    } else if req.to_uppercase().contains("PATCH") {
+        return Some(RequestType::PATCH);
+    } else if req.to_uppercase().contains("PUT") {
+        return Some(RequestType::PUT);
+    } else if req.to_uppercase().contains("HEAD") {
+        return Some(RequestType::HEAD);
+    } else if req.to_uppercase().contains("OPTIONS") {
+        return Some(RequestType::OPTIONS);
+    }
+    None
+}
+
+impl fmt::Display for RequestType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            RequestType::GET => f.write_str("GET"),
+            RequestType::POST => f.write_str("POST"),
+            RequestType::PATCH => f.write_str("PATCH"),
+            RequestType::DELETE => f.write_str("DELETE"),
+            RequestType::PUT => f.write_str("PUT"),
+            RequestType::HEAD => f.write_str("HEAD"),
+            RequestType::OPTIONS => f.write_str("OPTIONS"),
+        }
+    }
+}
+
 // site version
 // pub fn parse_request(request: &mut String) -> Result<Request, ()> {
 //     let mut parts = request.split(" ");
